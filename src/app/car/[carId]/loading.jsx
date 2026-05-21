b0eebaa2dfa2e51596c187e97d7388e414a00ddc
Md Shamim Hasan <mdshamimhasan791@gmail.com>
2026-05-21 16:46:14 +0600

    added loading

---
+++ src/app/car/[carId]/loading.jsx
@@ -0,0 +1,9 @@
+import { FadeLoader } from "react-spinners";
+const loading = () => {
+  return (
+    <div className="flex justify-center items-center">
+      <FadeLoader />
+    </div>
+  );
+};
+export default loading;
