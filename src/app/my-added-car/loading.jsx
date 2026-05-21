067d643d90acb53cb49fce80aa8a43e406ad498c
Md Shamim Hasan <mdshamimhasan791@gmail.com>
2026-05-21 16:49:12 +0600

    all complete frontend

--- src/app/my-added-car/loading.jsx
+++ src/app/my-added-car/loading.jsx
@@ -3,7 +3,7 @@ import { FadeLoader } from "react-spinners";
 
 const loading = () => {
   return (
-    <div className="flex justify-center items-center">
+    <div className="flex justify-center items-center min-h-screen">
       <FadeLoader />
     </div>
   );
